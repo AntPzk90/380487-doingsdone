f848e29b53fd38334a4c5fb16d39e2a57b6766fe
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-02-18 22:56:28 +0200

    внёс правки по замечаниям наставника

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ VALUES
 ("paziukAnatolii@gmail.com", "1234", "Anatolii"),
 ("paziukAlexey@gmail.com", "1324", "Aleksey");
 
-INSERT INTO projects (name_project, autor)
+INSERT INTO projects (name_project, id_user)
 VALUES 
 ("Входящие", 1),
 ("Учеба", 1),
@@ -12,7 +12,7 @@ VALUES
 ("Домашние дела", 2),
 ("Авто", 2);
 
-INSERT INTO tasks (status, task_title, file, deadline, id_user, id_project )
+INSERT INTO tasks (status, title, file, deadline, id_user, id_project )
 VALUES
 (0, "Собеседование в IT компании", "", "12.02.2019", 1, 3),
 (0, "Выполнить тестовое задание", "", "25.12.2019", 1, 3),
@@ -22,10 +22,10 @@ VALUES
 (0, "Заказать пиццу", "", "", 2, 4);
 
 /*получить список из всех проектов для одного пользователя*/
-SELECT t.task_title FROM tasks t WHERE t.id_user = 1;
+SELECT t.title FROM tasks t WHERE t.id_user = 1;
 /*получить список из всех задач для одного проекта*/
-SELECT t.task_title FROM tasks t WHERE t.id_project = 3;
+SELECT t.title FROM tasks t WHERE t.id_project = 3;
 /*отметить задачу как выполненную*/
-UPDATE tasks SET status = 1 WHERE id_code = 1;
+UPDATE tasks SET status = 1 WHERE id = 1;
 /*обновить название задачи по её идентификатору*/
-UPDATE tasks SET task_title = 'Покормил кота' WHERE id_code = 6;
+UPDATE tasks SET title = 'Покормил кота' WHERE id = 6;
